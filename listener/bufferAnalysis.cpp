290d60f3cdc2b3feddc2029105bf8912cf08e839
Ilias TRAGHA <104205016+BDF172@users.noreply.github.com>
2023-12-31 15:23:39 +0100

    Optimization
    Now, the conversion from bytes to integer for the audio analysis takes 60% less time, no cuts can be heard in the recordings anymore

--- listener/bufferAnalysis.cpp
+++ listener/bufferAnalysis.cpp
@@ -3,25 +3,28 @@
 using namespace std;
 
 int convertFromBytesToInteger(BYTE byte1, BYTE byte2, BYTE byte3, BYTE byte4){
-    unsigned char result[4] = {byte1, byte2, byte3, byte4};
+    int result = 0;
 
-    // int intValue = 0;
-    // for (int i = 3; i >= 0; i--) {
-    //     intValue = (intValue << 8) | result[i];
-    // }
+    result |= static_cast<int>(byte4) << 24;
+    result |= static_cast<int>(byte3) << 16;
+    result |= static_cast<int>(byte2) << 8;
+    result |= static_cast<int>(byte1);
 
-    // return intValue;
-    return *(reinterpret_cast<int *>(result));
+    return result;
 }
 
 void convertFromBufferToIntegerTab(BYTE* buffer, int bufferSize, int* conversionTab){
     for (int i = 0; i<bufferSize; i = i + 3) {
-        int test = convertFromBytesToInteger(buffer[i],buffer[i+1], buffer[i+2], 0x00);
-        *(conversionTab+(int)(i/3)) = test;
+        *(conversionTab+(int)(i/3)) = convertFromBytesToInteger(buffer[i],buffer[i+1], buffer[i+2], 0x00);
     }
 }
 
 unsigned int getAverageFromTab(unsigned int* bufferTab, int bufferSize){
+    if (bufferSize <= 0) {
+        // Gestion du cas où le tableau est vide pour éviter une division par zéro.
+        std::cerr << "Erreur: Buffer vide" << std::endl;
+        return 0;
+    }
     unsigned long long sum = 0;
     for (int i = 0; i<bufferSize; i++){
         sum += bufferTab[i];
